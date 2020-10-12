ee417edbd14c55ba14083ab5142c9fb09b20cb85
DrizzlingCattus <hit0473@gmail.com>
2020-10-13 01:44:31 +0900

    [chore] README 추가

---
+++ README.md
@@ -0,0 +1,3 @@
+## Reference
+
+- 예제 데이터 출처 및 문서: https://www.mariadbtutorial.com/getting-started/mariadb-sample-database/
--- run-playground.sh
+++ run-playground.sh
@@ -5,4 +5,4 @@ ENV_FILE=.env.dev
 source $ENV_FILE
 export $(cut -d= -f1 $ENV_FILE)
 
-cat docker-compose.yaml | envsubst | docker-compose -p "personal" -f - up -d --force-recreate --no-build
+cat docker-compose.yaml | envsubst | docker-compose -p "personal-sql" -f - up -d --force-recreate --no-build
